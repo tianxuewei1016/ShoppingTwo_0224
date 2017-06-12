36867930a7b1a55c0954957ac66cc51199044145
tianxuewei1016 <93226539@qq.com>
2017-06-12 11:50:31 +0800

    请求主页数据和解决数据

---
+++ app/src/main/java/com/atguigu/shoppingtwo_0224/activity/MyApplication.java
@@ -0,0 +1,31 @@
+package com.atguigu.shoppingtwo_0224.activity;
+
+import android.app.Application;
+
+import com.zhy.http.okhttp.OkHttpUtils;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * 作者：田学伟 on 2017/6/12 11:37
+ * QQ：93226539
+ * 作用：
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+//                .addInterceptor(new LoggerInterceptor("TAG"))
+                .connectTimeout(5000L, TimeUnit.MILLISECONDS)
+                .readTimeout(5000L, TimeUnit.MILLISECONDS)
+                //其他配置
+                .build();
+
+        OkHttpUtils.initClient(okHttpClient);
+    }
+}
--- app/src/main/java/com/atguigu/shoppingtwo_0224/home/fragment/HomeFragment.java
+++ app/src/main/java/com/atguigu/shoppingtwo_0224/home/fragment/HomeFragment.java
@@ -1,12 +1,24 @@
 package com.atguigu.shoppingtwo_0224.home.fragment;
 
-import android.graphics.Color;
-import android.util.Log;
-import android.view.Gravity;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.alibaba.fastjson.JSON;
+import com.atguigu.shoppingtwo_0224.R;
 import com.atguigu.shoppingtwo_0224.base.BaseFragment;
+import com.atguigu.shoppingtwo_0224.home.bean.HomeBean;
+import com.atguigu.shoppingtwo_0224.utils.Constants;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+import okhttp3.Call;
 
 /**
  * 作者：田学伟 on 2017/6/11 15:14
@@ -15,15 +27,23 @@ import com.atguigu.shoppingtwo_0224.base.BaseFragment;
  */
 
 public class HomeFragment extends BaseFragment {
-    private TextView textView;
+
+    @InjectView(R.id.ll_main_scan)
+    LinearLayout llMainScan;
+    @InjectView(R.id.tv_search_home)
+    TextView tvSearchHome;
+    @InjectView(R.id.tv_message_home)
+    TextView tvMessageHome;
+    @InjectView(R.id.rv_home)
+    RecyclerView rvHome;
+    @InjectView(R.id.ib_top)
+    ImageButton ibTop;
 
     @Override
     public View initView() {
-        textView = new TextView(mContext);
-        textView.setTextSize(20);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(mContext, R.layout.fragment_home, null);
+        ButterKnife.inject(this, view);
+        return view;
     }
 
     /**
@@ -33,7 +53,52 @@ public class HomeFragment extends BaseFragment {
     @Override
     public void initData() {
         super.initData();
-        Log.e("TAG", "主页的数据被初始化了...");
-        textView.setText("主页内容");
+        getDataFromNet();
+    }
+
+    private void getDataFromNet() {
+        OkHttpUtils.get()
+                .url(Constants.HOME_URL)
+                .id(100)
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+//                        Log.e("TAG", "联网失败==" + e.getMessage());
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+//                        Log.e("TAG", "联网成功==");
+                        processData(response);
+                    }
+                });
+    }
+
+    private void processData(String json) {
+        HomeBean homeBean = JSON.parseObject(json, HomeBean.class);
+//        Log.e("TAG", "解析数据成功==" + homeBean.getResult().getHot_info().get(0).getName());
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
+    }
+
+    @OnClick({R.id.tv_search_home, R.id.tv_message_home, R.id.ib_top})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_search_home:
+                Toast.makeText(mContext, "搜索", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_message_home:
+                Toast.makeText(mContext, "查看", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ib_top:
+                Toast.makeText(mContext, "回到顶部", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
