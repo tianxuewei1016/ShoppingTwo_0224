df2ad64726b70ffccea7e632dc070c37a82fba2a
tianxuewei1016 <93226539@qq.com>
2017-06-12 09:52:49 +0800

    Initial commit

---
+++ app/src/main/java/com/atguigu/shoppingtwo_0224/WelcomeActivity.java
@@ -0,0 +1,47 @@
+package com.atguigu.shoppingtwo_0224;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+
+import com.atguigu.shoppingtwo_0224.activity.MainActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startMainActivity();
+            }
+        }, 2000);
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        //关闭当前页面
+        finish();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            startMainActivity();
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        new Handler().removeCallbacksAndMessages(null);
+    }
+}
