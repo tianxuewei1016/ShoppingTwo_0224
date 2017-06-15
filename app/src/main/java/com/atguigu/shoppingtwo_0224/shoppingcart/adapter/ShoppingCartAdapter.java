f823b6fa03b6404c4dcc8ce1b194c776501cde07
tianxuewei1016 <93226539@qq.com>
2017-06-15 09:57:35 +0800

    适配器ShoppingCartAdapter

--- app/src/main/java/com/atguigu/shoppingtwo_0224/shoppingcart/adapter/ShoppingCartAdapter.java
+++ app/src/main/java/com/atguigu/shoppingtwo_0224/shoppingcart/adapter/ShoppingCartAdapter.java
@@ -50,11 +50,16 @@ public class ShoppingCartAdapter extends RecyclerView.Adapter<ShoppingCartAdapte
         //1.更加位置得到数据
         GoodsBean goodsBean = datas.get(position);
         //2.绑定数据
+        holder.cbGov.setChecked(goodsBean.isChecked());
         Glide.with(mContext)
                 .load(Constants.BASE_URL_IMAGE + goodsBean.getFigure()).into(holder.ivGov);
         holder.tvDescGov.setText(goodsBean.getName());
         //设置价格
         holder.tvPriceGov.setText("￥" + goodsBean.getCover_price());
+
+        holder.AddSubView.setValue(goodsBean.getNumber());
+        holder.AddSubView.setMinValue(1);
+        holder.AddSubView.setMaxValue(20);
     }
 
 
@@ -73,6 +78,18 @@ public class ShoppingCartAdapter extends RecyclerView.Adapter<ShoppingCartAdapte
         public MyViewHolder(View itemView) {
             super(itemView);
             ButterKnife.inject(this, itemView);
+            //设置item的点击事件
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //状态取反
+                    GoodsBean goodsBean = datas.get(getLayoutPosition());
+                    goodsBean.setChecked(!goodsBean.isChecked());
+
+                  //刷新适配器
+                    notifyItemChanged(getLayoutPosition());
+                }
+            });
         }
     }
 }
